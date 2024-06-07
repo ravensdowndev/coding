62226e5a5a274c3bb782323b36a576b14f4e8b8c
simon.bishop.ravd@outlook.com <simon.bishop.ravd@outlook.com>
2024-06-07 16:10:16 +1200

    Last change to program - handle exceptions.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,7 +8,14 @@ namespace SpreadyMcSpreader
         static void Main(string[] args)
         {
             var calculator = new SimpleCalculator();
-            Console.WriteLine(calculator.CalculateStatistics(args[0]));
+            try
+            {
+                Console.WriteLine(calculator.CalculateStatistics(args[0]));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An Error occurred: {ex.Message}");
+            }
         }
     }
 }
